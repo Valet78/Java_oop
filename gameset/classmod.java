ec7a62dd19705b58a6d1938348f2d33af0aa1ecc
Valet78 <valet78@mail.ru>
2023-02-15 14:11:46 +0300

    New GameSet

---
+++ gameset/classmod.java
@@ -0,0 +1,5 @@
+package gameset;
+
+public class classmod {
+    
+}
---
+++ gameset/program.java
@@ -0,0 +1,12 @@
+package gameset;
+
+
+public class program {
+    public static void main(String[] args) {
+
+
+        
+        
+    }
+    
+}
